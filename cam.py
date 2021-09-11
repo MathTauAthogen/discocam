93156f20740440d7052f9ce1d9b12afdc4e5ade3
Andrew Mascillaro <amascillaro@gmail.com>
2021-09-11 14:46:03 -0400

    Add rickroll

--- cam.py
+++ cam.py
@@ -44,6 +44,14 @@ class Cam:
             "filter": "[v]rotate=\'PI*" + params[1][1] + "*sin(2*PI*t/" + params[0][1] + ")\'[v]",
         })
 
+    def add_rickroll(self, params):
+        params = self.parse_input(params, ["position"], ["topright"])
+
+        self.effects.append({
+            "name": "rickroll",
+            "inputs": ["static/rickroll.mp4"],
+            "filter": "[#####][v]scale2ref=iw*0.25:-1[rickroll][v];[v][rickroll]overlay=x=W*2/3:y=H/12[v]"
+        })
     
     def add_rotate(self, angle="PI"):
         self.effects.append({
@@ -75,7 +83,7 @@ class Cam:
             num_inputs = len(effect["inputs"])
             current_filter = effect["filter"]
             if num_inputs == 1:
-                current_filter = current_filter.replace("#####", next_input)
+                current_filter = current_filter.replace("#####", str(next_input))
                 next_input += num_inputs # 1
             elif num_inputs > 1:
                 pass # NOT IMPLEMENTED
@@ -85,7 +93,7 @@ class Cam:
         
         ## Generate command
         self.command = "ffmpeg "
-        self.command += "-i " + "-i ".join(self.inputs) + " "
+        self.command += "-i " + " -i ".join(self.inputs) + " "
         if self.filters:
             self.command += "-filter_complex \""
             self.command += ";".join(self.filters)
@@ -140,6 +148,8 @@ if __name__ == "__main__":
                 cam.add_colorcycle(words[2:])
             elif function == "rock":
                 cam.add_rock(words[2:])
+            elif function == "rickroll":
+                cam.add_rickroll(words[2:])
             else:
                 print("function not recognized")
         elif words[0] == "remove":
